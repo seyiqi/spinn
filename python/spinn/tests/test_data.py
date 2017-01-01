2897d1fe2e1d3c65c1d4817234ffb9bb502d3832
Andrew Drozdov <andrew@mrdrozdov.com>
2017-01-01 15:19:11 -0500

    Test case for boolean data

--- python/spinn/tests/test_data.py
+++ python/spinn/tests/test_data.py
@@ -7,6 +7,7 @@ from spinn import util
 from spinn.data.snli import load_snli_data
 from spinn.data.sst import load_sst_data
 from spinn.data.arithmetic import load_simple_data
+from spinn.data.boolean import load_boolean_data
 from collections import Counter
 
 
@@ -33,6 +34,7 @@ from the sentence padding token). Transitions are padded on the left.
 snli_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_snli.jsonl")
 sst_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_sst.txt")
 arithmetic_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_simple.tsv")
+boolean_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_boolean.tsv")
 embedding_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_embedding_matrix.5d.txt")
 word_embedding_dim = 5
 
@@ -160,7 +162,7 @@ class SNLITestCase(unittest.TestCase):
 
         tokens, transitions, labels, num_transitions = data
         
-        # Filter pairs that don't have both hyp and prem transition length <= seq_length
+        # Filter examples that don't have lengths <= seq_length
         assert tokens.shape == (2, seq_length, 2)
         assert transitions.shape == (2, seq_length, 2)
 
@@ -224,7 +226,7 @@ class SSTTestCase(unittest.TestCase):
 
         tokens, transitions, labels, num_transitions = data
 
-        # Filter pairs that don't have both hyp and prem transition length <= seq_length
+        # Filter examples that don't have lengths <= seq_length
         assert tokens.shape == (4, seq_length)
         assert transitions.shape == (4, seq_length)
 
@@ -274,7 +276,7 @@ class ArithmeticTestCase(unittest.TestCase):
 
         tokens, transitions, labels, num_transitions = data
 
-        # Filter pairs that don't have both hyp and prem transition length <= seq_length
+        # Filter examples that don't have lengths <= seq_length
         assert tokens.shape == (16, seq_length)
         assert transitions.shape == (16, seq_length)
 
@@ -282,6 +284,56 @@ class ArithmeticTestCase(unittest.TestCase):
 
             # The sentence should begin with an operator.
             assert any(s[0] == tkn for tkn in OPERATOR_TOKENS)
+
+            # The sentences should be padded on the right.
+            assert not s_is_left_padded(s)
+
+            # The num_transitions should count non-skip transitions
+            assert len([x for x in ts if x != util.SKIP_SYMBOL]) == num_t
+
+            # The transitions should start with SKIP and end with REDUCE (ignoring SKIPs).
+            assert t_is_left_to_right(ts)
+
+            # The transitions should be padded on the left.
+            assert t_is_left_padded(ts)
+
+
+class BooleanTestCase(unittest.TestCase):
+
+    def test_load(self):
+        # NOTE: Boolean tsv file uses a tab between label and example.
+        data_manager = load_boolean_data
+        raw_data, _ = data_manager.load_data(boolean_data_path)
+        assert len(raw_data) == 20
+
+        seq_lengths = Counter([len(x['transitions']) for x in raw_data])
+        assert seq_lengths == {29: 7, 21: 5, 27: 3, 25: 3, 23: 2}
+
+    def test_preprocess(self):
+        seq_length = 24
+        for_rnn = False
+        use_left_padding = True
+
+        data_manager = load_boolean_data
+        raw_data, vocabulary = data_manager.load_data(boolean_data_path)
+
+        OPERATOR_TOKENS = [vocabulary["and"], vocabulary["or"]]
+
+        data = util.PreprocessDataset(
+            raw_data, vocabulary, seq_length, data_manager, eval_mode=False, logger=MockLogger(),
+            sentence_pair_data=data_manager.SENTENCE_PAIR_DATA,
+            for_rnn=for_rnn, use_left_padding=use_left_padding)
+
+        tokens, transitions, labels, num_transitions = data
+
+        # Filter examples that don't have lengths <= seq_length
+        assert tokens.shape == (7, seq_length)
+        assert transitions.shape == (7, seq_length)
+
+        for s, ts, num_t in zip(tokens, transitions, num_transitions):
+
+            # The sentence should end with an operator.
+            assert s_is_left_to_right(s, OPERATOR_TOKENS)
 
             # The sentences should be padded on the right.
             assert not s_is_left_padded(s)
