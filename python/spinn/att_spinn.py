1bb0034c02c57d61ef1997ae2065d9adaa8ecaa8
Xiaonan <xiaonan@nyu.edu>
2017-02-21 09:23:58 -0500

    basic strucutre of att_spinn

--- python/spinn/att_spinn.py
+++ python/spinn/att_spinn.py
@@ -2,15 +2,60 @@ import numpy as np
 from torch import nn
 
 
-class BaseModel(nn.Module):
+class SentencePairTrainer():
+    """
+    required by the framework, fat_classifier.py @291,295
+    init as classifier_trainer at fat_classifier.py @337
+    """
     pass
 
 
+class AttentionModel():
 
-class SentencePairModel(BaseModel):
+    def __init__(self):
+        # default define, TODO
+        pass
 
+    def soft_attention(self, stack1, ):
+        """
+
+        :param stack1:
+        :param stack2:
+        :return:
+        """
+
+class BaseClassifierModel(nn.module):
+
+    def __init__(self):
+    """
+    """
+    pass
+
+    def forward(self):
+        """
+
+        :return: likelihood []
+        """
+        return [-1, -1, -1] # TODO
+
+
+class SentencePairModel(BaseClassifierModel):
+    """
+    required by the framework, fat_classifier.py@296
+    init as model at fat_classifier.py@300
+    """
     def __init__(self):
         pass
 
     def forward(self, *input):
         pass
+
+class SentenceModel(BaseClassifierModel):
+    """
+    required by the framework, fat_classifier.py@296
+    init as model at fat_classifier.py@300
+    because attention model take two sentences, this model might never be used
+    """
+    pass
+
+
