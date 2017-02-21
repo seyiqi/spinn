7ddef95095deb45c9f8c06fbe9f361254662eafd
Alex <xiaonan0429@gmail.com>
2017-02-20 21:24:09 -0500

    Basic strucutre of a new model

---
+++ python/spinn/att_spinn.py
@@ -0,0 +1,16 @@
+import numpy as np
+from torch import nn
+
+
+class BaseModel(nn.Module):
+    pass
+
+
+
+class SentencePairModel(BaseModel):
+
+    def __init__(self):
+        pass
+
+    def forward(self, *input):
+        pass
